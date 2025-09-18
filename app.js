9e5728432b1056a2999e23cf11641ed059158587
camshiieee <beldacamilleshainne8@gmail.com>
2025-09-18 16:17:20 +0800

    updated a file

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
+const standardShippingCost = 6;
+const discountedShippingCost = 4;
 let totalPrice;
 
 function calculateShippingCost(totalPriceParam) {
     let shippingCost;
     
     if (totalPriceParam <= 10) {
-        shippingCost = 5;
+        shippingCost = standardShippingCost
     } else if (totalPriceParam <= 20) {
-        shippingCost = 3;
+        shippingCost = discountedShippingCost
     } else {
         shippingCost = 0;
     }
 
     console.log('shipping cost for you is 3')
     console.log('for total price of 19')
+    console.log("---------------------------")
 }
 
 // user starts shopping
-totalPrice = 10
+totalPrice = 10;
+calculateShippingCost(totalPrice)
+
 // more items added to shopping cart
-totalPrice = 19
+totalPrice = 19;
 //ready to order
 calculateShippingCost(totalPrice)
+
+// add another thing to cart
+totalPrice = 24;
+calculateShippingCost(totalPrice)
+
+// place the order
