5464582d2e5296fd319a822007a701c7e66c872f
camshiieee <beldacamilleshainne8@gmail.com>
2025-09-22 10:37:45 +0800

    conditionals file

---
+++ PHP/04_conditionals.php
@@ -0,0 +1,53 @@
+<?php
+
+// if-else + comparison operators
+// --
+// ---
+// !- or <>
+// !-
+// <
+// <=
+// >
+// >=
+$age = 14;
+
+if ($age <= 12) {
+    echo "Too young to watch Harry Potter";
+} else if ($age < 15 ) {
+    echo " You may watch Harry Potter";
+} else {
+     echo " You should watch Harry Potter";
+}
+
+// Logical Operators
+// && - and
+// || - or 
+// - not
+$age = 15;
+$watched = true;
+
+if ($age >= 15 && !$watched) {
+    echo 'You MUST watch Harry Potter';
+}
+
+if ($age >= 15 || !watched) {
+    echo 'You should watch Harry Potter';
+}
+
+// Swicth
+$day = 3;
+
+switch ($day) {
+        case 1:
+            echo "Monday";
+            break;
+        case 2:
+            echo "Tuesaday";
+            break;
+        case 3:
+            echo "Wednesday";
+            break;
+        default:
+            echo "Invalid day";
+      
+}
